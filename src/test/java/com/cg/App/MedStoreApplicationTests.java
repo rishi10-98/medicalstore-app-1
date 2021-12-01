53228f1d5b095ab9fb6a71e15b14e3691f29f0b7
rishi10-98 <82711305+rishi10-98@users.noreply.github.com>
2021-12-01 14:52:23 +0530

    Update MedStoreApplicationTests.java

--- src/test/java/com/cg/App/MedStoreApplicationTests.java
+++ src/test/java/com/cg/App/MedStoreApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest(classes = MedStoreApplication.class)
 
 class MedStoreApplicationTests {
