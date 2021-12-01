ecc356405a7d312ecfedfac95bebb0c36da18fee
rishi10-98 <82711305+rishi10-98@users.noreply.github.com>
2021-12-01 14:21:30 +0530

    Update MedStoreApplicationTests.java

--- src/test/java/com/cg/App/MedStoreApplicationTests.java
+++ src/test/java/com/cg/App/MedStoreApplicationTests.java
@@ -3,10 +3,10 @@ package com.cg.App;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.cg.App.controller.Hello;
+
 
 @SpringBootTest(classes = MedStoreApplication.class)
 
