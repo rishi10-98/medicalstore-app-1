6f556282d48ab2f7a62369eeb1f5cbcbd4fdee8b
rishi10-98 <82711305+rishi10-98@users.noreply.github.com>
2021-12-01 14:46:58 +0530

    Update Hello.java

--- src/main/java/com/cg/App/controller/Hello.java
+++ src/main/java/com/cg/App/controller/Hello.java
@@ -10,19 +10,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
-//	private static final 
-	Logger LOG = LoggerFactory.getLogger(Hello.class);
-
-	// URL - http://localhost:8084/hello
-	@RequestMapping("/") // endpoint, API
-	public String wel() {
-		LOG.info("Hello world!");
-		return "Wlcome !";
-	}
-
-	@RequestMapping("/hello") // endpoint, API
-	public String hello() {
-		LOG.info("Hello world!");
-		return "Hello world!";
-	}
 }
