3d8911a3853c677f40c6b485571f51500ed9e390
pratikkashid <pratikkashid11@gmail.com>
2021-12-01 13:14:02 +0530

    Update MedStoreApplicationTests.java

--- src/test/java/com/cg/App/MedStoreApplicationTests.java
+++ src/test/java/com/cg/App/MedStoreApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.cg.App.controller.Hello;
 
 @SpringBootTest
-@WebMvcTest(Hello.class)
+//@WebMvcTest(Hello.class)
 class MedStoreApplicationTests {
 
 //	@Test
